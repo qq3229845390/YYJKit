fe7cdea2f3c09cfcb2f8b32dbc446afc138e7181
杨英俊 <3229845390@qq.com>
2020-08-21 16:48:08 +0800

    init

---
+++ YYJKit/category/NSString+YYJ.h
@@ -0,0 +1,17 @@
+//
+//  NSString+YYJ.h
+//  YYJKitDemo
+//
+//  Created by belltrip on 2020/8/21.
+//  Copyright © 2020 yangyingjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (YYJ)
+
+@end
+
+NS_ASSUME_NONNULL_END
